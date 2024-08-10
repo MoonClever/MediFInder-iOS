be3b2278e4e47b270fe9a54bfd5c1912a39730f1
Jordi Cuevas <j.o.cuevas53@gmail.com>
2024-08-10 02:03:25 -0600

    Initial Commit

---
+++ MediFInderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediFInderApp
+//
+//  Created by Octavio on 10/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
